6e22eb2e2e5e5747bec310f1a4382722e4477efb
emmapankey <emmapankey19@gmail.com>
2017-10-28 18:46:27 -0400

    final

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    
+
     // Array of topics to use to query the Giphy API
     var topics = [
         "Cheeseburger",
@@ -10,88 +10,88 @@ $(document).ready(function () {
         "Ice Cream",
         "Burrito"
     ];
-    
-        var searchTerm = "";
- 
-        // Function to iterate through the topics array and create a button for each item.
-        function renderButtons() {
-            $("#buttons-appear-here").empty();
-            for (var i = 0; i < topics.length; i++) {
-                var a = $("<button class='btn btn-default' id='topicButtons'>").text(topics[i]);
-                a.addClass("food");
-                a.attr("data-name", topics[i]);
-                $("#buttons-appear-here").prepend(a);
+
+    var searchTerm = "";
+
+    // Function to iterate through the topics array and create a button for each item.
+    function renderButtons() {
+        $("#buttons-appear-here").empty();
+        for (var i = 0; i < topics.length; i++) {
+            var a = $("<button class='btn btn-default' id='topicButtons'>").text(topics[i]);
+            a.addClass("food");
+            a.attr("data-name", topics[i]);
+            $("#buttons-appear-here").prepend(a);
+        }
+    }
+
+    // Function with AJAX call to the Giphy API. Data for still and animated is added to the returned gifs.
+    function displayGif() {
+
+        var foodItem = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            foodItem + "&api_key=AV3UsdwQLJ2J3scDVcBiFH9YuhCn32qH&limit=10";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).done(function (response) {
+            console.log(response);
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+
+                var staticURL = results[i].images.fixed_height_still.url;
+                var animatedURL = results[i].images.fixed_height.url;
+
+                var rating = results[i].rating;
+                var ratingP = $("<p id='ratings'>").text("Rating: " + rating);
+
+                var gifImage = $("<img id='gifs'>");
+
+                gifImage.attr("src", staticURL);
+                gifImage.attr("data-state", "still");
+                gifImage.attr("data-still", staticURL);
+                gifImage.attr("data-animate", animatedURL);
+
+                $("#gifs-appear-here").prepend(gifImage);
+                $("#gifs-appear-here").prepend(ratingP);
             }
-        }
+        });
+    }
 
-        // Function with AJAX call to the Giphy API. Data for still and animated is added to the returned gifs.
-        function displayGif() {
+    // Click event for the submit button which creates a button for the search term input by the user.
+    $("#addButton").on("click", function () {
+        searchTerm = $("#userInput").val()
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=AV3UsdwQLJ2J3scDVcBiFH9YuhCn32qH&limit=10";
 
-                var foodItem = $(this).attr("data-name");
-                var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-                    foodItem + "&api_key=AV3UsdwQLJ2J3scDVcBiFH9YuhCn32qH&limit=10";
-    
-                $.ajax({
-                    url: queryURL,
-                    method: "GET"
-                }).done(function (response) {
-                    console.log(response);
-                    var results = response.data;
-    
-                    for (var i = 0; i < results.length; i++) {
-    
-                        var staticURL = results[i].images.fixed_height_still.url;
-                        var animatedURL = results[i].images.fixed_height.url;
-    
-                        var rating = results[i].rating;
-                        var ratingP = $("<p id='ratings'>").text("Rating: " + rating);
-    
-                        var gifImage = $("<img id='gifs'>");
-                
-                        gifImage.attr("src", staticURL);
-                        gifImage.attr("data-state", "still");
-                        gifImage.attr("data-still", staticURL);
-                        gifImage.attr("data-animate", animatedURL);
-    
-                        $("#gifs-appear-here").prepend(gifImage);
-                        $("#gifs-appear-here").prepend(ratingP);
-                    }
-                });
-        }
-    
-        // Click event for the submit button which creates a button for the search term input by the user.
-            $("#addButton").on("click", function () {
-                searchTerm = $("#userInput").val()
-                var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=AV3UsdwQLJ2J3scDVcBiFH9YuhCn32qH&limit=10";
-    
-                var a = $("<button class='btn btn-default' id='topicButtons'>").text(searchTerm);
-                a.addClass("food");
-                a.attr("data-name", searchTerm);
-                $("#buttons-appear-here").prepend(a);
+        var a = $("<button class='btn btn-default' id='topicButtons'>").text(searchTerm);
+        a.addClass("food");
+        a.attr("data-name", searchTerm);
+        $("#buttons-appear-here").prepend(a);
 
-                topics.push(searchTerm);
+        topics.push(searchTerm);
 
-                renderButtons();
-            });
-
-        // Function for changing the gifs to their still or animated state.
-        function playStopGifs() {
-                var state = $(this).attr("data-state");
-                console.log(this);
-                if (state === "still") {
-                    $(this).attr("src", $(this).attr("data-animate"));
-                    $(this).attr("data-state", "animate");
-                } else {
-                    $(this).attr("src", $(this).attr("data-still"));
-                    $(this).attr("data-state", "still");
-                }
-        
-        }
-
-        $(document).on("click", "#gifs", playStopGifs);
-        
-        $(document).on("click", ".food", displayGif);
-    
         renderButtons();
-    
+    });
+
+    // Function for changing the gifs to their still or animated state.
+    function playStopGifs() {
+        var state = $(this).attr("data-state");
+        console.log(this);
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+
+    }
+
+    $(document).on("click", "#gifs", playStopGifs);
+
+    $(document).on("click", ".food", displayGif);
+
+    renderButtons();
+
 });
